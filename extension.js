d9b06bc9d13d94e7ff621a9f6f63917a442af4bf
nassir <daniya_n@yahoo.com>
2021-05-04 19:08:55 -0700

    readme modification

--- extension.js
+++ extension.js
@@ -29,13 +29,10 @@ function activate(context) {
       vscode.window.showInformationMessage("Hello World from ATTMN!");
       //--------------------------------------------------------------------------------------------------------------------------
       const config = {
-
-        projectId: "fifth-medley-312204",,
+        projectId: "fifth-medley-312204",
         keyFilename:
-       "C:\\Users\\12064\\Documents\\Project-ATTMN\\fifth-medley-312204-9ce88867a9ba.json",
+        "C:\\Users\\12064\\Desktop\\fifth-medley-312204-3de415f7b035.json",
 
-
-       
       };
       const client = new speech.SpeechClient(config);
 
